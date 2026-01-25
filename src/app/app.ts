40d0fc4c040d4b46ea51637137930fb6c1b62c2b
Maxiim3 <maximemickael.contact@gmail.com>
2026-01-25 14:18:08 +0100

    add socket test in front

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PORT, ROUTES } from '../../shared/constants';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('jumble-chat');
+
+  constructor() {
+    this.connect();
+  }
+
+  connect() {
+    const ws = new WebSocket(`ws://localhost:${PORT}${ROUTES.ws}`);
+
+    ws.onopen = () => console.log('Connecté');
+    ws.send('coucou');
+    ws.onmessage = (e) => console.log('Message', e);
+    ws.onclose = () => console.log('Déconnecté');
+  }
 }
